5f8cb378118879f29cfec32690833492afb5ba65
gniehues <gniehues90@gmail.com>
2017-12-01 20:40:06 -0200

    unit tests added to controller

---
+++ src/test/java/intellead/ControllerTest.java
@@ -0,0 +1,59 @@
+/*
+ *
+ * Copyright 2017 Softplan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+package intellead;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ControllerTest {
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Controller controller;
+
+    @Test
+    public void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void testAll() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("intellead-security"));
+    }
+
+}
